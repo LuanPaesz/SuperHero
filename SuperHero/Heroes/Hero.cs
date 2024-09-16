9bc9ae3a210d43823d07f5d897f898b0bceca822
2023357 <2023357@student.cct.ie>
2024-09-16 15:21:59 +0100

    Add project files.

---
+++ SuperHero/Heroes/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero.Heroes
+{
+    public class Hero
+    { public int Id { get; set; }
+        public string HeroName { get; set; }
+        public string RealName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{HeroName} : {RealName}";
+        }
+
+    }
+}
---
+++ SuperHero/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SuperHero.Heroes;
+using System;
+
+Console.WriteLine("Hello, World!");
+
+string heroName = "SpiderMan";
+string realName = "Piter Parker";
+
+
+Hero p = new Hero();
+p.HeroName = heroName;
+p.RealName = realName;
+
+Hero p1 = new Hero();
+p1.HeroName = "Batman";
+p1.RealName = "Bruce Wayne";
+
+Hero p2 = new Hero();
+p2.HeroName = "Hulk";
+p2.RealName = "David Banner";
+
+List<Hero> list = new List<Hero>();
+list.Add(p);
+list.Add(p1);
+list.Add(p2);
+
+foreach (Hero hero in list)
+{
+    //Console.WriteLine($"Hello, my hero name is {p1.HeroName}! but, my real name is {p1.RealName}");
+    Console.WriteLine(hero);
+
+}
+
